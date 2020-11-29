c3f65b62c7b86fd825fd3da0e5fb2e2adec27246
Bruno Manfredi <arthias.schatten@gmail.com>
2020-11-29 20:38:16 -0300

    basic setup

---
+++ Terminal_Hacker/Assets/WM2000/hacker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What wuld you like to hack into?");
+        Terminal.WriteLine("Press 1 for library");
+        Terminal.WriteLine("Press 2 for police station");
+        Terminal.WriteLine("Please enter your selection:");
+    }
+}
