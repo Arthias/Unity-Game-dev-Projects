62138d630b1cf328f90b6638e96115e36e2c25c0
Bruno Manfredi <arthias.schatten@gmail.com>
2020-12-03 16:16:45 -0300

    menu changes

--- Terminal_Hacker/Assets/WM2000/hacker.cs
+++ Terminal_Hacker/Assets/WM2000/hacker.cs
@@ -4,24 +4,62 @@ using UnityEngine;
 
 public class hacker : MonoBehaviour
 {
+    int level;
+    enum Screen { Menu, Level, Win };
+    Screen currentScreen;
     // Start is called before the first frame update
     void Start()
     {
+        level = 0;
         ShowMainMenu();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void ShowMainMenu()
     {
+        currentScreen = Screen.Menu;
         Terminal.ClearScreen();
-        Terminal.WriteLine("What wuld you like to hack into?");
+        Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for library");
         Terminal.WriteLine("Press 2 for police station");
         Terminal.WriteLine("Please enter your selection:");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            Start();
+        }
+        else if (currentScreen == Screen.Menu)
+        {
+            MainMenu(input);
+        }
+
+    }
+
+    void MainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please select a Valid objective");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Level;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Please enter Password");
+
     }
 }
